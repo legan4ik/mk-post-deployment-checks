edd6201a03c7d0d48ad9d1f951232219083f3d77
Oleksii Zhurba <legan4ik@gmail.com>
2017-09-25 21:31:20 +0000

    Fixing test_mtu

--- mk_verificator/tests/test_mtu.py
+++ mk_verificator/tests/test_mtu.py
@@ -1,4 +1,5 @@
 import pytest
+import json
 from mk_verificator import utils
 
 
@@ -8,58 +9,58 @@ from mk_verificator import utils
 )
 def test_mtu(local_salt_client, group):
     config = utils.get_configuration(__file__)
-
     skipped_ifaces = config["skipped_ifaces"]
-    expected_mtu = config["expected_mtu"]
-
     total = {}
-    failed_ifaces = {}
-
     network_info = local_salt_client.cmd(
-        group, 'cmd.run', ['sudo ls /sys/class/net/'], expr_form='pcre')
+        group, 'cmd.run', ['ls /sys/class/net/ | grep -v "veth" | grep -v "cali"'], expr_form='pcre')
 
     kvm_nodes = local_salt_client.cmd(
         'salt:control', 'test.ping', expr_form='pillar').keys()
 
+    if len(network_info.keys()) < 2:
+        pytest.skip("Nothing to compare - only 1 node")
+
     for node, ifaces_info in network_info.iteritems():
         if node in kvm_nodes:
             kvm_info = local_salt_client.cmd(node, 'cmd.run',
-                                             ["virsh list | grep jse2 | "
+                                             ["virsh list | "
                                               "awk '{print $2}' | "
                                               "xargs -n1 virsh domiflist | "
                                               "grep -v br-pxe | grep br- | "
                                               "awk '{print $1}'"])
             ifaces_info = kvm_info.get(node)
-        group_name = node.split('-')[0]
         node_ifaces = ifaces_info.split('\n')
-        if group_name not in expected_mtu:
-            continue
-        else:
-            ifaces = {}
-            for iface in node_ifaces:
-                if iface in skipped_ifaces:
-                    continue
-                iface_mtu = local_salt_client.cmd(node, 'cmd.run',
+        ifaces = {}
+        for iface in node_ifaces:
+            if iface in skipped_ifaces:
+                continue
+            iface_mtu = local_salt_client.cmd(node, 'cmd.run',
                                                   ['cat /sys/class/'
                                                    'net/{}/mtu'.format(iface)])
-                ifaces[iface] = iface_mtu.get(node)
-            total[node] = ifaces
+            ifaces[iface] = iface_mtu.get(node)
+        total[node] = ifaces
+
+    nodes = []
+    mtu_data = []
+    my_set = set()
 
     for node in total:
-        ifaces = total.get(node)
-        group_name = node.split('-')[0]
-
-        for iface in ifaces:
-            if group_name not in expected_mtu:
-                continue
+        nodes.append(node)
+        my_set.update(total[node].keys())
+    for interf in my_set:
+        diff = []
+        row = []
+        for node in nodes:
+            if interf in total[node].keys():
+                diff.append(total[node][interf])
+                row.append("{}: {}".format(node, total[node][interf]))
             else:
-                group = expected_mtu.get(group_name)
-                gauge = group.get(iface)
-                mtu = ifaces.get(iface)
-                if iface not in expected_mtu:
-                    continue
-                elif int(mtu) != int(gauge):
-                    failed_ifaces[node].append(iface)
+                row.append("{}: No interface".format(node))
+        if diff.count(diff[0]) < len(nodes):
+            row.sort()
+            row.insert(0, interf)
+            mtu_data.append(row)
+    assert len(mtu_data) == 0, \
+        "Several problems found for {0} group: {1}".format(
+        group, json.dumps(mtu_data, indent=4))
 
-    assert not failed_ifaces, "Nodes with " \
-                              "iface mismatch: {}".format(failed_ifaces)
